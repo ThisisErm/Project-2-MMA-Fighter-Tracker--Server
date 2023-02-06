1acfb220f5e37bc61f033ef0c7d407cbc2544d2f
bethparnell <beth.parnell@generalassemb.ly>
2023-02-06 14:01:31 -0500

    code review completed

--- Server/models/skill.js
+++ Server/models/skill.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 
 const skillSchema = new Schema(
     {
+        // Remove the [] around this String. We just want a single string not many strings
         title: [String],
     }
 )
